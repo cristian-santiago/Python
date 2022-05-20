7325c339849a1c404c3556b1104f033dbbe2c49c
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-05-20 17:32:24 -0300

    Create mf.py

---
+++ Python-codes-CeV/mf.py
@@ -0,0 +1,11 @@
+'''
+My functions that I created to support me during my lessons.
+
+'''
+
+def title(msg):
+    #This function will show up a title covered by two lines, one above and other below the msg
+    print('-'*30)
+    print(msg)
+    print('-'*30)
+    
